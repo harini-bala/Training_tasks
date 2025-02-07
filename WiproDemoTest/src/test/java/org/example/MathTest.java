3b775a96d983713f4a68aebaba12c3d274a8a7db
harini-bala <99002432+harini-bala@users.noreply.github.com>
2025-02-07 22:14:59 +0530

    Add files via upload

---
+++ WiproDemoTest/src/test/java/org/example/MathTest.java
@@ -0,0 +1,11 @@
+package org.example;
+import junit.framework.Assert;
+import org.junit.*;
+
+public class MathTest {
+    public static void main(String[] args) {
+        Assert.assertEquals(7, Math.sum(2,5));
+    }
+
+    }
+}
